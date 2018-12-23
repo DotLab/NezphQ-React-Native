2d3fe54436d814d13694fa1afe2782477ee6e24a
Kailang <kailangfu@gmail.com>
2018-12-22 22:32:46 -0600

    Finish loop

--- App.js
+++ App.js
@@ -9,15 +9,21 @@ import { generateSecureRandom } from 'react-native-securerandom';
 const forge = require("node-forge");
 const nacl = require('tweetnacl');
 
+const encode = forge.util.binary.raw.encode;
+const decode = forge.util.binary.raw.decode;
+const sha256 = forge.md.sha256.create;
+
 export default class App extends React.Component {
 	constructor() {
 		super();
-		this.state = { loading: "generating key" };
+		this.state = { loading: ":nezph q:" };
 	}
 
 	componentDidMount() {
-		generateSecureRandom(2048).then(randomBytes => {
-			forge.random.collect(forge.util.binary.raw.encode(randomBytes));
+		this.setState({ loading: "finding entropy" });
+		// feed entropy
+		generateSecureRandom(4089).then(randomBytes => {
+			forge.random.collect(encode(randomBytes));
 			nacl.setPRNG((x, n) => {
 				const str = forge.random.getBytesSync(n);
 				for (var i = 0; i < n; i += 1) {
@@ -25,6 +31,8 @@ export default class App extends React.Component {
 				}
 			});
 
+			// generate rsa
+			this.setState({ loading: "generating key" });
 			return new Promise((resolve, reject) => {
 				setTimeout(() => {
 					forge.pki.rsa.generateKeyPair(512, 0x10001, (err, rsa) => {
@@ -43,93 +51,38 @@ export default class App extends React.Component {
 
 	onReconnectButtonPress() {
 		this.setState({ loading: "connecting" });
+		
 		this.socket = io("http://localhost:6021");
-		this.socket.on("disconnect", () => {
-			this.setState({ error: "broken pipe" });
-		})
+		this.socket.on("disconnect", () => { this.setState({ error: "broken pipe" }); })
+		
 		new Promise(resolve => {
 			this.socket.on("connect", resolve);
 		}).then(() => {
 			// x25519.publicKey: gx1
 			// x25519PublicKeySignature: Sign(gx1, kA)
 			this.x25519 = nacl.box.keyPair();
-			this.x25519PublicKeyBase64 = forge.util.binary.base64.encode(this.x25519.publicKey);
-				// forge.util.encode64(new forge.util.ByteStringBuffer(this.x25519.publicKey).bytes());
-				// naclUtil.encodeBase64(this.x25519.publicKey);
-			const hasher = forge.md.sha256.create().update(this.x25519PublicKeyBase64, "utf8");
-			this.x25519PublicKeySignature = this.rsa.privateKey.sign(hasher);
-
+			const x25519PublicKey = encode(this.x25519.publicKey);
+			const hasher = sha256().update(x25519PublicKey, "raw");
+			const x25519PublicKeySignature = this.rsa.privateKey.sign(hasher);
 			this.aesIv = forge.random.getBytesSync(32);
 
-			this.setState({ loading: "handshaking", error: null });
+			this.setState({ loading: "handshaking", error: undefined });			
 			return new Promise(resolve => {
 				this.socket.emit("cl_handshake", { 
 					rsaPem: forge.pki.publicKeyToPem(this.rsa.publicKey),  // KA 
-					x25519PublicKeyBase64: this.x25519PublicKeyBase64,  // gx1
-					x25519PublicKeySignature: this.x25519PublicKeySignature,  // Sign(gx1, kA),
+					x25519PublicKey, x25519PublicKeySignature,
 					aesIv: this.aesIv
 				}, resolve);
 			});
 		}).then(roomId => {
 			this.roomId = roomId;
-			this.setState({ loading: null });
+			this.setState({ loading: undefined });
 			
 			const socket = this.socket;
-			socket.on("sv_connected", (roomId, cred) => {
-				this.bobId = roomId;
-				this.bobCred = cred;
-				
-				// bobRsaPublicKey: KB
-				this.bobRsaPublicKey = forge.pki.publicKeyFromPem(cred.rsaPem)
-				var hash = forge.md.sha256.create().update(cred.x25519PublicKeyBase64, "utf8").digest().bytes();
-				var verified = this.bobRsaPublicKey.verify(hash, cred.x25519PublicKeySignature);
-				if (!verified) {
-					this.setState({ error: "false prophet" });
-					return Promise.reject(1);
-				}
-
-				// x25519SharedSecret: gx1y1
-				// aesKey: k11 = H(gx1y1)
-				this.x25519SharedSecret = nacl.box.before(
-					forge.util.binary.base64.decode(cred.x25519PublicKeyBase64),
-					// forge.util.decode64(new forge.util.ByteStringBuffer(cred.x25519PublicKeyBase64).bytes()),
-					this.x25519.secretKey);
-					// naclUtil.decodeBase64(cred.x25519PublicKeyBase64), this.x25519.secretKey);
-				this.aesKey = forge.md.sha256.create().update(
-					forge.util.binary.base64.encode(this.x25519SharedSecret),
-					// forge.util.encode64(new forge.util.ByteStringBuffer(this.x25519SharedSecret).bytes()), 
-					"utf8").digest().bytes();
-					// naclUtil.encodeBase64(this.x25519SharedSecret), "utf8").digest().bytes();
-				this.hmacKey = forge.md.sha256.create().update(this.aesKey).digest().bytes();
-
-				const cipher = forge.cipher.createCipher("AES-CTR", this.aesKey);
-				cipher.start({ iv: this.aesIv });
-				cipher.update(forge.util.createBuffer("test", "utf8"));
-				cipher.finish();
-				var encrypted = cipher.output;
-				
-				const decipher = forge.cipher.createDecipher("AES-CTR", this.aesKey);
-				decipher.start({ iv: cred.aesIv });
-				decipher.update(encrypted);
-				const res = decipher.finish();
-				Alert.alert(decipher.output.toString());
-				
-				const hmac = forge.hmac.create();
-				hmac.start("sha256", this.hmacKey);
-				hmac.update("123");
-
-				this.setState({ messages: [ {
-					_id: 1,
-					text: "a secure channel has been established",
-					createdAt: new Date(),
-					system: true
-				} ] });
-			});
-
-			socket.on("sv_deliver", msg => {
-				Alert.alert(msg);
-			});
+			socket.on("sv_connected", this.onSocketSvConnected.bind(this));
+			socket.on("sv_deliver", this.onSocketSvDeliver.bind(this));
 		}).catch(err => {
+			Alert.alert(err.message);
 			if (err !== 1) throw err;
 		});
 	}
@@ -144,10 +97,98 @@ export default class App extends React.Component {
 		});
 	}
 
-	onSend(messages) {
-		console.log(messages);
+	onSocketSvConnected(roomId, cred) {
+		// bobRsaPublicKey: KB
+		const bobRsaPublicKey = forge.pki.publicKeyFromPem(cred.rsaPem)
+		var hash = sha256().update(cred.x25519PublicKey, "raw").digest().bytes();
+		var verified = bobRsaPublicKey.verify(hash, cred.x25519PublicKeySignature);
+		if (!verified) {
+			this.setState({ error: "false prophet" });
+			return Promise.reject(1);
+		}
+		this.bobAesIv = cred.aesIv;
+
+		// x25519SharedSecret: gx1y1
+		// aesKey: k11 = H(gx1y1)
+		// hmacKey: H(k11)
+		const x25519SharedSecret = nacl.box.before(decode(cred.x25519PublicKey), this.x25519.secretKey);
+		this.aesKey = sha256().update(encode(x25519SharedSecret), "raw").digest().bytes();
+		this.hmacKey = sha256().update(this.aesKey).digest().bytes();
+
+		this.setState({ messages: [ {
+			_id: 1,
+			text: "a secure channel has been established",
+			createdAt: new Date(),
+			system: true
+		} ] });
+	}
+
+	onSendButtonPress(messages) {
+		if (!messages || !messages[0] || !messages[0].text) return;
+		const text = messages[0].text;
+
+		new Promise(resolve => {
+			setTimeout(() => {
+				this.setState({ title: "sending..." });
+
+				// next x25519 pair
+				// x25519PublicKey: gxi+1
+				const x25519 = nacl.box.keyPair();
+				const x25519PublicKey = encode(x25519.publicKey);
+
+				// encryption
+				// ciphertext: E(M,kij)
+				const cipher = forge.cipher.createCipher("AES-CTR", this.aesKey);
+				cipher.start({ iv: this.aesIv });
+				cipher.update(forge.util.createBuffer(text, "utf8"));
+				cipher.finish();
+				const ciphertext = cipher.output.bytes();
+				
+				// mac = MAC({gxi+1,E(M,kij)},H(kij))
+				const hasher = forge.hmac.create();
+				hasher.start("sha256", this.hmacKey);
+				hasher.update(x25519PublicKey);
+				hasher.update(ciphertext);
+				const mac = hasher.getMac().bytes();
+
+				this.socket.emit("cl_send", { x25519PublicKey, ciphertext, mac }, resolve);
+			}, 0);
+		}).then(() => {
+			this.setState(prevState => ({
+				title: undefined,
+				messages: GiftedChat.append(prevState.messages, messages),
+			}));
+		});
+	}
+
+	processMessage(msg) {
+		var text = "[damaged]";
+
+		const hasher = forge.hmac.create();
+		hasher.start("sha256", this.hmacKey);
+		hasher.update(msg.x25519PublicKey);
+		hasher.update(msg.ciphertext);
+		const mac = hasher.getMac().bytes();
+		if (mac !== msg.mac) return text;
+
+		const decipher = forge.cipher.createDecipher("AES-CTR", this.aesKey);
+		decipher.start({ iv: this.bobAesIv });
+		decipher.update(forge.util.createBuffer(msg.ciphertext, "raw"));
+		const res = decipher.finish();
+		if (!res) return text;
+
+		return decipher.output.toString();
+	}
+
+	onSocketSvDeliver(msg) {
+		text = this. processMessage(msg);
 		this.setState(prevState => ({
-			messages: GiftedChat.append(prevState.messages, messages),
+			messages: GiftedChat.append(prevState.messages, {
+				_id: Math.round(Math.random() * 1000000),
+				text,
+				createdAt: new Date(),
+				user: { _id: 2, name: 'sinner' },
+			}),
 		}));
 	}
 
@@ -179,14 +220,14 @@ export default class App extends React.Component {
 						</Nb.Button>
 					</Nb.Left>
 					<Nb.Body>
-						<Nb.Title>sinner</Nb.Title>
+						<Nb.Title>{state.title || "sinner"}</Nb.Title>
 					</Nb.Body>
 					<Nb.Right />
 				</Nb.Header>
 				<View style={{ flex: 1 }}>
 					<GiftedChat
 						messages={state.messages} 
-						onSend={this.onSend.bind(this)} 
+						onSend={this.onSendButtonPress.bind(this)} 
 						user={{ _id: 1 }}
 					/>
 				</View>
